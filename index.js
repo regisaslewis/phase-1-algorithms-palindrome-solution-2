06ad45c61428259426ca34b0babcc436361cd38d
Regis <regisaslewis@gmail.com>
2023-02-28 23:03:59 -0800

    palindrome lab solution 2

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+// Write your algorithm here
+//   iterate from the beginning to the middle of the word
+//   get the length of the string and divide by two, round down for the middle index
+for (let i = 0; i < word.length / 2; i++) {
+  //   check each letter to the corresponding letter from the end
+  const j = word.length - 1 -i;
+
+//     if any letters don't match, return false
+  if (word[i] !== word[j]) return false;
+}
+// return true
+return true;
 }
 
 /* 
   Add your pseudocode here
+
+  Make a function that returns true if the word is a palindrome.  That means if the first letter is the same as the last letter and the second letter is the same as the second to last letter, etc. until we reach the middle, return true.
+
+  abba
+  ^  ^
+   ^^
+  true
+
+  robot
+  ^   x
+  false
+
+  iterate from the beginning to the middle of the word
+    check each letter to the corresponding letter from the end
+      if any letters don't match, return false
+  
+  return true
 */
 
 /*
@@ -20,6 +48,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
 }
 
 module.exports = isPalindrome;
